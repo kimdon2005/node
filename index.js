4e9ac03c3b5d981a8c5b5697248abb452f494a43
kimdon2005 <kimdon2005@naver.com>
2022-09-10 21:57:43 +0900

    set routing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
 });
 
